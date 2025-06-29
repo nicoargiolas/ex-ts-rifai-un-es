0399eccdbe797f6575f4aac7d398c1239903728c
nicoargiolas <argiolas.nicola00@gmail.com>
2025-06-29 14:32:14 +0200

    Creazione type alias e type guard per Chef

--- src/main.ts
+++ src/main.ts
@@ -46,6 +46,7 @@ function isRecipe(dati: unknown): dati is Recipe {
         "cookTimeMinutes" in dati && typeof dati.cookTimeMinutes === "number" &&
         "cuisine" in dati && typeof dati.cuisine === "string" &&
         "difficulty" in dati && typeof dati.difficulty === "string" &&
+        (dati.difficulty === "Easy" || dati.difficulty === "Medium" || dati.difficulty === "Difficult") &&
         "id" in dati && typeof dati.id === "number" &&
         "image" in dati && typeof dati.image === "string" &&
         "ingredients" in dati && dati.ingredients instanceof Array && dati.ingredients.every(i => typeof i === "string") &&
@@ -58,6 +59,76 @@ function isRecipe(dati: unknown): dati is Recipe {
         "servings" in dati && typeof dati.servings === "number" &&
         "tags" in dati && dati.tags instanceof Array && dati.tags.every(t => typeof t === "string") &&
         "userId" in dati && typeof dati.userId === "number"
+    ) {
+        return true
+    } else {
+        return false
+    }
+}
+
+type Chef = {
+    address: object,
+    age: number,
+    bank: object,
+    birthDate: string,
+    bloodGroup: string,
+    company: object,
+    crypto: object,
+    ein: string,
+    email: string,
+    eyeColor: string,
+    readonly firstName: string,
+    gender: "male" | "female",
+    hair: object,
+    height: number,
+    readonly id: number,
+    image: string,
+    ip: string,
+    readonly lastName: string,
+    macAddress: string,
+    maidenName: string,
+    password: string,
+    phone: string,
+    role: string,
+    ssn: string,
+    university: string,
+    userAgent: string,
+    readonly username: string,
+    weight: number
+}
+
+function isChef(dati: unknown): dati is Chef {
+    if (
+        dati && typeof dati === "object" && dati !== null &&
+        "address" in dati && typeof dati.address === "object" && dati.address !== null &&
+        "age" in dati && typeof dati.age === "number" &&
+        "bank" in dati && typeof dati.bank === "object" && dati.bank !== null &&
+        "birthDate" in dati && typeof dati.birthDate === "string" &&
+        "bloodGroup" in dati && typeof dati.bloodGroup === "string" &&
+        "company" in dati && typeof dati.company === "object" && dati.company !== null &&
+        "crypto" in dati && typeof dati.crypto === "object" && dati.crypto !== null &&
+        "ein" in dati && typeof dati.ein === "string" &&
+        "email" in dati && typeof dati.email === "string" &&
+        "eyeColor" in dati && typeof dati.eyeColor === "string" &&
+        "firstName" in dati && typeof dati.firstName === "string" &&
+        "gender" in dati && typeof dati.gender === "string" &&
+        (dati.gender === "male" || dati.gender === "female") &&
+        "hair" in dati && typeof dati.hair === "object" && dati.hair !== null &&
+        "height" in dati && typeof dati.height === "number" &&
+        "id" in dati && typeof dati.id === "number" &&
+        "image" in dati && typeof dati.image === "string" &&
+        "ip" in dati && typeof dati.ip === "string" &&
+        "lastName" in dati && typeof dati.lastName === "string" &&
+        "macAddress" in dati && typeof dati.macAddress === "string" &&
+        "maidenName" in dati && typeof dati.maidenName === "string" &&
+        "password" in dati && typeof dati.password === "string" &&
+        "phone" in dati && typeof dati.phone === "string" &&
+        "role" in dati && typeof dati.role === "string" &&
+        "ssn" in dati && typeof dati.ssn === "string" &&
+        "university" in dati && typeof dati.university === "string" &&
+        "userAgent" in dati && typeof dati.userAgent === "string" &&
+        "username" in dati && typeof dati.username === "string" &&
+        "weight" in dati && typeof dati.weight === "number"
     ) {
         return true
     } else {
